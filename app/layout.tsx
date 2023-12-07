b77075c8ed1347fcc41bfb01c21c1169575dbccd
edgarolivar16 <edgarolivar16@gmail.com>
2023-12-07 10:34:04 -0300

    end course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
 import { inter } from './ui/fonts';
+
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+
 
 export default function RootLayout({
   children,
@@ -7,9 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}</body>
+    <html lang='en'>
+      <head>
+        <meta property='og:title' content='Cotizador de Seguros' />
+        <meta property='og:description' content='Panel dinamico para administrar y cotizar seguros automotriz' />
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
